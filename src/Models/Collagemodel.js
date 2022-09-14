c93af3cdf960b1d1fefb212a6e0e67db9fddfd13
ashanka710 <ashankad343@gmail.com>
2022-09-14 11:25:09 +0530

    ashanka

---
+++ src/Models/Collagemodel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+const CollageSchema = mongoose.schema({
+    name: {
+        type: string,
+        required: true,
+        unique: true,
+    },
+    fullname: {
+        type: string,
+        required: true,
+    },
+    logoLink: {
+        require: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamp: true })
+mondule.export = mongoose.model('Collage', CollageSchema)
